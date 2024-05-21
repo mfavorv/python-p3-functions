69299fcbb52d1b808f733a33854b81f14b2ece10
Favor Vincent <vfavorm77@gmail.com>
2024-05-21 03:22:22 +0300

    python-functions lab complete

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+greet("Jean")    
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()
 
 def add(num1, num2):
     pass
+    return num1 + num2
+add(1, 2)
 
 def halve(number):
     pass
+    return number / 2
+halve(16)
